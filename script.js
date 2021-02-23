757c14e721335e8942179e3bcb3770224dc3ba1f
Your Name <stasgill@gmail.com>
2021-02-23 19:54:39 +0200

    new

---
+++ script.js
@@ -0,0 +1,31 @@
+
+// Первое задание
+
+// let makeProfileTimer = function(){
+//     let counter = performance.now()
+//     return counter
+// };
+
+// let start = makeProfileTimer()
+  
+// alert('Замеряем время работы этого alert');  
+// let resultCounter = makeProfileTimer() - start
+// alert(resultCounter); 
+  
+// ===============================================
+
+// const makeProfileTimer = function () {
+//     let start = 6
+//     let counter = 0
+//     return  () =>  start - ++counter  ;
+//       };
+  
+// let shit  = makeProfileTimer()
+   
+     
+//   let timerId = setInterval(() => console.log(shit()), 1000);
+
+//   let other = setTimeout(() => { clearInterval(timerId); alert('stop'); }, 5000);
+  
+
+  
